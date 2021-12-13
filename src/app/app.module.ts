0b0f7dd0eccb3e32f7e4dae97db6aaa5a72947cc
franxiscussteven <84694556+franxiscussteven@users.noreply.github.com>
2021-12-13 21:38:31 +0700

    edit post sehat
    1. perbaiki tampilan detail
    2. revisi add webservis add sedikit
    3. sekalian insert like 0 lgsg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AddpostComponent } from './addpost/addpost.component';
 import { DetailpostComponent } from './detailpost/detailpost.component';
 import { KomentarComponent } from './komentar/komentar.component';
 import { AddkomentarComponent } from './addkomentar/addkomentar.component';
+import { EditpostComponent } from './editpost/editpost.component';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { Routes, RouterModule} from '@angular/router';
@@ -25,10 +26,11 @@ const appRoutes:Routes = [
   {path:'detail/:id' , component:DetailpostComponent},
   {path:'komentar/:id' , component:KomentarComponent},
   {path:'addkomentar/:id' , component:AddkomentarComponent},
+  {path:'editpost/:id' , component:EditpostComponent},
 ]
 
 @NgModule({
-  declarations: [AppComponent, RegisterComponent, MypostComponent, AddpostComponent, DetailpostComponent, KomentarComponent, AddkomentarComponent],
+  declarations: [AppComponent, RegisterComponent, MypostComponent, AddpostComponent, DetailpostComponent, KomentarComponent, AddkomentarComponent, EditpostComponent],
   entryComponents: [],
   imports: [IonicStorageModule.forRoot(), HttpClientModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [Camera, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
--- src/app/editpost/editpost.component.ts
+++ src/app/editpost/editpost.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router'
+import { Storage } from '@ionic/storage';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-editpost',
@@ -7,8 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditpostComponent implements OnInit {
 
-  constructor() { }
+  constructor(public ps:PostService, private http: HttpClient, public route:ActivatedRoute, private storage:Storage) { }
 
-  ngOnInit() {}
+  id:number=this.route.snapshot.params['id'];
+  iduser = 0;
+
+  judul:string = "";
+  isi:string = "";
+  tanggal:string = "";
+  gambar:string = "";
+  total:number = 0;
+  username:string = "";
+  judulPost: string = "";
+  isiPost: string = "";
+
+
+  currentpost = [];
+
+  detailPost(id){
+    this.ps.postDetail(id).subscribe(
+      (data)=> {
+        this.judul = data['judul'];
+        this.isi = data['isi'];
+        this.tanggal = data['tanggal'];
+        this.gambar = data['gambar'];
+        this.total = data['total_like'];
+        this.username = data['username'];
+	    }
+	  );
+  };
+
+  edit(judul: string, isi: string): Observable<any> {
+    let body = new HttpParams();
+    body = body.set('judul', judul);
+    body = body.set('isi', isi);
+    body = body.set('post_id', this.id);
+    return this.http.post("https://ubaya.fun/hybrid/160719019/UAS/editpost.php", body);
+  }
+
+  editPost() {
+    this.edit(this.judulPost, this.isiPost).subscribe(
+      (data) => {
+        alert(data['pesan']);
+      });
+  }
+
+  async ngOnInit() {
+    await this.storage.create();
+    this.iduser = await this.storage.get('iduser');
+    this.detailPost(this.id);
+  }
 
 }
